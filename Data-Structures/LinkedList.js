fc829a74b79b11566f320b7fda51d9d4ca018ebc
Derick Park <derickmpark@gmail.com>
2020-02-22 15:56:56 -0800

    Implement LinkedLists (addToHead, removeHead, addToTail, removeTail, contains, removeAnywhere)

--- Data-Structures/LinkedList.js
+++ Data-Structures/LinkedList.js
@@ -12,18 +12,9 @@ class LinkedList {
   }
   addToHead = input => {
     var node = new Node(input);
-    var current;
-    if (!this.head) { 
-      this.head = node;
-    } else { 
-      current = this.head; 
-    }
-    while (current && current.next) { 
-      current = current.next;
-    } 
-    if (current) {
-      current.next = node;
-    }
+    this.tail = new Node(input);
+    node.next = this.head;
+    this.head = node;
   }
   removeHead = () => {
     if (this.head === null) { 
@@ -32,6 +23,28 @@ class LinkedList {
     var currentHead = this.head;
     this.head = this.head.next;
     return currentHead.value;
+  }
+  addToTail = input => {
+    var node = new Node(input);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      var current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+      this.tail = node;
+    }
+  }
+  removeTail = () => {
+    var current = this.head;
+    while (current.next.next) {
+      current = current.next;
+    }
+    current.next = null;
+    this.tail = current;
+  
   }
   contains = target => {
     var current = this.head;
@@ -43,15 +56,39 @@ class LinkedList {
     } 
     return false;
   }
+  removeAnywhere = target => {
+    var current = this.head;
+    var previous = null;
+    while (current) {
+      if (current.value === target) {
+        if (!previous) {
+          this.head = current.next;
+        } else {
+          previous.next = current.next;
+        }
+        if (!current.next) {
+          this.tail = previous;
+        }
+      }
+      previous = current;
+      current = current.next;
+    }
+  }
 }
 
 // var LList = new LinkedList;
 // LList.addToHead(1);
+// LList.addToHead(5);
 // LList.addToHead(6);
-// LList.addToHead(6);
-// LList.addToHead(1);
+// LList.addToHead(7);
 // LList.removeHead();
+// LList.addToTail(1);
+// LList.addToTail(5);
+// LList.addToTail(2);
+// LList.addToTail(7);
+// LList.removeTail();
+// LList.removeAnywhere(7);
 // console.log(LList.contains(6));
-// console.log(LList);
+// console.log(JSON.stringify(LList));
 
-module.exports = LinkedList;
+// module.exports = LinkedList;
