48e623553b95712e755abeffd053c717fab848ee
Derick Park <derickmpark@gmail.com>
2020-02-21 22:27:17 -0800

    Implement BinarySearchTree (insert, search)

--- Data-Structures/BinarySearchTree.js
+++ Data-Structures/BinarySearchTree.js
@@ -13,14 +13,14 @@ class BinarySearchTree {
           current.right = new BinarySearchTree(input);
           break;
         } else {
-          current = current.right
+          current = current.right;
         }
       } else {
         if (!current.left) {
-          current.left = new BinarySearchTree(input)
+          current.left = new BinarySearchTree(input);
           break;
         } else {
-          current = current.left
+          current = current.left;
         }
       }
     }
@@ -41,6 +41,36 @@ class BinarySearchTree {
     //   }
     // }
   }
+  search(input) {
+    // While
+    var current = this;
+    while (current) {
+      if (current.value === input) {
+        return true;
+      }
+      if (input < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+    
+    // Recursion
+    // var current = this;
+    // if (current.value === input) {
+    //   return true;
+    // }
+    // if (input < current.value) {
+    //   if (current.left) {
+    //     return current.left.search(input) || false;
+    //   }
+    // } else {
+    //   if (current.right) {
+    //     return current.right.search(input) || false;
+    //   }
+    // } 
+  }
 }
 
 var BST = new BinarySearchTree(6)
@@ -49,5 +79,6 @@ BST.insert(7)
 BST.insert(5)
 BST.insert(10)
 BST.insert(3)
-
+console.log('ME', BST.search(2))
+console.log('ME', BST.search(3))
 console.log(BST);
