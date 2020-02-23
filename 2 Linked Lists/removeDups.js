3496bd31a28be6988b7907d009acd611bb9af19e
Derick Park <derickmpark@gmail.com>
2020-02-22 18:50:13 -0800

    Implement LinkedList data structure for testing

--- 2 Linked Lists/removeDups.js
+++ 2 Linked Lists/removeDups.js
@@ -1,4 +1,11 @@
+const LinkedList = require('../Data-Structures/LinkedList')
+
 // Remove Dups: Write code to remove duplicates from an unsorted linked list.
+
+// O: LinkedList
+// I: LinkedList
+// C: N/A
+// E: N/A
 
 2.1
 function removeDups(linkedList) {
@@ -7,18 +14,27 @@ function removeDups(linkedList) {
   var previous = linkedList.head;
   while(current) {
     if (!obj[current.value]) {
-      obj[current.value] = current
-      previous = current
+      obj[current.value] = current;
+      previous = current;
     } else {
-      previous.next = current.next
+      previous.next = current.next;
     }
-    current = current.next
+    linkedList.tail = current
+    current = current.next;
   }
   return linkedList;
 }
 
 // Time: O(n)
 // Space: O(n)
+
+// var LList = new LinkedList();
+// LList.addToHead(1);
+// LList.addToHead(6);
+// LList.addToHead(6);
+// LList.addToHead(10);
+
+// console.log(JSON.stringify(removeDups(LList)));
 
 // console.log(removeDups(
 //   {
