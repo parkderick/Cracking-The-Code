913c678a49a89cee7d9d679ec1cd9b2f0aed4472
Derick Park <derickmpark@gmail.com>
2020-02-23 23:10:01 -0800

    Implement BinarySearchTree (insert, search)

--- Data-Structures/BinarySearchTree.js
+++ Data-Structures/BinarySearchTree.js
@@ -5,7 +5,6 @@ class BinarySearchTree {
     this.right = null;
   };
   insert(input) {
-    // While
     var current = this;
     while (current) {
       if (input > current.value) {
@@ -24,25 +23,8 @@ class BinarySearchTree {
         }
       }
     }
-
-    // Recursion
-    // var current = this;
-    // if (input > current.value) {
-    //   if (!current.right) {
-    //     current.right = new BinarySearchTree(input);
-    //   } else {
-    //     current.right.insert(input);
-    //   }
-    // } else {
-    //   if (!current.left) {
-    //     current.left = new BinarySearchTree(input);
-    //   } else {
-    //     current.left.insert(input);
-    //   }
-    // }
   }
   search(input) {
-    // While
     var current = this;
     while (current) {
       if (current.value === input) {
@@ -55,21 +37,6 @@ class BinarySearchTree {
       }
     }
     return false;
-    
-    // Recursion
-    // var current = this;
-    // if (current.value === input) {
-    //   return true;
-    // }
-    // if (input < current.value) {
-    //   if (current.left) {
-    //     return current.left.search(input) || false;
-    //   }
-    // } else {
-    //   if (current.right) {
-    //     return current.right.search(input) || false;
-    //   }
-    // } 
   }
 }
 
@@ -82,3 +49,44 @@ BST.insert(3)
 console.log('ME', BST.search(2))
 console.log('ME', BST.search(3))
 console.log(BST);
+
+
+
+// class BinarySearchTree {
+//   constructor(input) {
+//     this.value = input;
+//     this.left = null;
+//     this.right = null;
+//   };
+//   insert(input) {
+//     var current = this;
+//     if (input > current.value) {
+//       if (!current.right) {
+//         current.right = new BinarySearchTree(input);
+//       } else {
+//         current.right.insert(input);
+//       }
+//     } else {
+//       if (!current.left) {
+//         current.left = new BinarySearchTree(input);
+//       } else {
+//         current.left.insert(input);
+//       }
+//     }
+//   }
+//   search(input) {
+//     var current = this;
+//     if (current.value === input) {
+//       return true;
+//     }
+//     if (input < current.value) {
+//       if (current.left) {
+//         return current.left.search(input) || false;
+//       }
+//     } else {
+//       if (current.right) {
+//         return current.right.search(input) || false;
+//       }
+//     } 
+//   }
+// }
