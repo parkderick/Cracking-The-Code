62e297826ec41f963dc76ed23da28f86a13b71bd
Derick Park <derickmpark@gmail.com>
2020-02-25 15:50:38 -0800

    Implement test using LinkedList (loopDetection)

--- 2 Linked Lists/loopDetection.js
+++ 2 Linked Lists/loopDetection.js
@@ -1,9 +1,16 @@
+const {LinkedList} = require('../Data-Structures/LinkedList');
+
 // Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
 // DEFINI TION
 // Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
 // EXAMPLE
 // Input: A -) B -) C -) 0 -) E -) C[thesameCasearlierl Output: C
 // Hints: #50, #69, #83, #90
+
+// O: node
+// I: LinkedList
+// C: N/A
+// E: N/A
 
 2.8
 function loopDetection(linkedList) {
@@ -13,7 +20,7 @@ function loopDetection(linkedList) {
     if (obj[current.value]) {
       return obj[current.value]
     } else {
-      obj[current.value] = current.value;
+      obj[current.value] = current;
     }
     current = current.next
   }
@@ -23,23 +30,11 @@ function loopDetection(linkedList) {
 // Time: O(n)
 // Space: O(1)
 
-// console.log(loopDetection({
-//   head: {
-//     value: 1,
-//     next: {
-//       value: 10,
-//       next: {
-//         value: 6,
-//         next: {
-//           value: 2,
-//           next: {
-//             value: 1,
-//             next: {
-//               value: 3,
-//               next: null
-//             }        
-//           }          
-//       }
-//     }
-//   }
-// }}))
+var LList = new LinkedList();
+LList.addToHead(1);
+LList.addToHead(6);
+LList.addToHead(5);
+LList.addToHead(10);
+LList.addToHead(5);
+
+console.log(loopDetection(LList));
