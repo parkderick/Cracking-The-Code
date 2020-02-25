49bcf5255e704d18ef5af3e004738d2c22f0a13d
Derick Park <derickmpark@gmail.com>
2020-02-25 15:18:49 -0800

    Implement test using LinkedList (isPalindrome)

--- 2 Linked Lists/isPalindrome.js
+++ 2 Linked Lists/isPalindrome.js
@@ -1,4 +1,11 @@
+const {LinkedList} = require('../Data-Structures/LinkedList');
+
 // Palindrome: Implement a function to check if a linked list is a palindrome.
+
+// O: Boolean
+// I: LinkedList
+// C: N/A
+// E: N/A
 
 2.6
 function isPalindrome(linkedList) {
@@ -12,20 +19,14 @@ function isPalindrome(linkedList) {
   return palindrome === copy;
 }
 
-// console.log(isPalindrome({
-//   head: {
-//     value: 1,
-//     next: {
-//       value: 10,
-//       next: {
-//         value: 6,
-//         next: {
-//           value: 6,
-//           next: null          
-//       }
-//     }
-//   }
-// }}))
-
 // Time: O(n)
-// Space: O(1)
+// Space: O(n)
+
+/* TEST */
+var LList = new LinkedList();
+LList.addToHead(1);
+LList.addToHead(6);
+LList.addToHead(4);
+LList.addToHead(1);
+
+console.log(isPalindrome(LList));
