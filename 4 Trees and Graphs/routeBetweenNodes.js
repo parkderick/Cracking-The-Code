ee8fdae329c98c03ee94fe0ee0c9df7542b3e2e9
Derick Park <derickmpark@gmail.com>
2020-02-26 18:59:15 -0800

    Implement test using Graph (routeBetweenNodes)

--- 4 Trees and Graphs/routeBetweenNodes.js
+++ 4 Trees and Graphs/routeBetweenNodes.js
@@ -1,3 +1,5 @@
+const {Graph} = require('../Data-Structures/Graphs');
+
 // Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 // O: Boolean
 // I: graph, node1, node2
@@ -5,7 +7,7 @@
 // E: N/A
 
 4.1
-function RouteBetweenNodes (graph, node1, node2) {
+function routeBetweenNodes (graph, node1, node2) {
   if (graph.AdjList[node1].edges[node2] || graph.AdjList[node2].edges[node1]) {
     return true
   } else {
@@ -15,4 +17,16 @@ function RouteBetweenNodes (graph, node1, node2) {
 
 // Time: O(1)
 // Space: O(1)
+
+/* TEST */
+var graph = new Graph();
+graph.addVertex(1);
+graph.addVertex(6);
+graph.addVertex(8);
+graph.addVertex(10);
+graph.addEdge(1, 10);
+
+console.log(routeBetweenNodes(graph, 1, 10));
+console.log(routeBetweenNodes(graph, 10, 1));
+console.log(routeBetweenNodes(graph, 1, 6));
 
