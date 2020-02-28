700ed91715890ac3d1c4dc3c719afe2d3ddeaaec
Derick Park <derickmpark@gmail.com>
2020-02-27 21:43:50 -0800

    Implement test using BinarySearchTree (validateBST)

--- 4 Trees and Graphs/validateBST.js
+++ 4 Trees and Graphs/validateBST.js
@@ -1,4 +1,11 @@
+const BinarySearchTree = require('../Data-Structures/BinarySearchTree.js');
+
 // Validate BST: Implement a function to check if a binary tree is a binary search tree. Hints: #35, #57, #86, # 773, # 728
+
+// O: Boolean
+// I: binary tree
+// C: N/A
+// E: N/A
 
 4.5
 function validateBST(BT) {
@@ -7,8 +14,8 @@ function validateBST(BT) {
   }
   var root = BT.value;
   var bool = true;
-  var left = DFS(BT.left, 'left');
-  var right = DFS(BT.right, 'right');
+  DFS(BT.left, 'left');
+  DFS(BT.right, 'right');
 
   function DFS(node, direction) {
     if (direction === 'left') {
@@ -28,52 +35,63 @@ function validateBST(BT) {
   return bool
 }
 
-// ValidateBST({ 
-//   value: 6,
-//   left: { 
-//     value: 2,
-//     left: {
-//       value: 0,
-//       left: null,
-//       right: {
-//         value: 3,
-//         left: null,
-//         right: {
-//           value: 7,
-//           left: null,
-//           right: null
-//         }
-//       }
-//     },
-//     right: {
-//       value: 4,
-//       left: null,
-//       right: null
-//     }
-//   },
-//   right: {
-//     value: 7,
-//     left: null,
-//     right: {
-//       value: 10,
-//       left: {
-//         value: 8,
-//         left: null,
-//         right: {
-//           value: 9,
-//           left: null,
-//           right: {
-//             value: 12,
-//             left: null,
-//             right: null
-//           }
-//         }
-//       },
-//       right: {
-//         value: 11,
-//         left: null,
-//         right: null
-//       }
-//     }
-//   }
-// })
+/* TEST */
+var bst = new BinarySearchTree(6);
+bst.insert(1);
+bst.insert(2);
+bst.insert(7);
+bst.insert(9);
+bst.insert(5);
+bst.insert(4);
+bst.insert(11);
+console.log(validateBST(bst));
+
+console.log(validateBST({ 
+  value: 6,
+  left: { 
+    value: 2,
+    left: {
+      value: 0,
+      left: null,
+      right: {
+        value: 3,
+        left: null,
+        right: {
+          value: 7,
+          left: null,
+          right: null
+        }
+      }
+    },
+    right: {
+      value: 4,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: 7,
+    left: null,
+    right: {
+      value: 10,
+      left: {
+        value: 8,
+        left: null,
+        right: {
+          value: 9,
+          left: null,
+          right: {
+            value: 12,
+            left: null,
+            right: null
+          }
+        }
+      },
+      right: {
+        value: 11,
+        left: null,
+        right: null
+      }
+    }
+  }
+}));
