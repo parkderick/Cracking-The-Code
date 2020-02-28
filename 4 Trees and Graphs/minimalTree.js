9f0bcef6c0aeaed4ff4911bb4f11e499274aa53a
Derick Park <derickmpark@gmail.com>
2020-02-27 16:23:01 -0800

    Implement test using BinarySearchTree (minimalTree)

--- 4 Trees and Graphs/minimalTree.js
+++ 4 Trees and Graphs/minimalTree.js
@@ -1,3 +1,5 @@
+const BinarySearchTree = require('../Data-Structures/BinarySearchTree.js');
+
 // Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algo- rithm to create a binary search tree with minimal height.
 // O: Tree
 // I: array, height
@@ -5,30 +7,22 @@
 // E: N/A
 
 4.2
-class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
 // Solution 1
 function minimalTree(array, min) {
   var root = array[min];
-  var BST = new TreeNode(root);
+  var BST = new BinarySearchTree(root);
   var lTemp = BST;
   var rTemp = BST;
   for (var i = 0; i < array.length; i++) {
     if (i !== min) {
       if (array[i] > rTemp.value) {
-        rTemp.right = new TreeNode(array[i])
+        rTemp.right = new BinarySearchTree(array[i])
         rTemp = rTemp.right
       } else if (array[i] < lTemp.value) {
-        lTemp.left = new TreeNode(array[i])
+        lTemp.left = new BinarySearchTree(array[i])
         lTemp = lTemp.left;
       } else {
-        lTemp.right = new TreeNode(array[i])
+        lTemp.right = new BinarySearchTree(array[i])
         lTemp = lTemp.right;
       }
     }
@@ -36,25 +30,24 @@ function minimalTree(array, min) {
   return BST
 }
 
-
-// Time: O(n)
-// Space: O(n)
-
 // Solution 2
 function minimalTree(array, min) {
   var root = array[min];
-  var BST = new TreeNode(root);
-  BST.left = innerNodes(array.slice(0, min))
-  BST.right = innerNodes(array.slice(min + 1, array.length))
+  var BST = new BinarySearchTree(root);
   var innerNodes = (nodes, dir) => {
     if (nodes.length !== 0) {
-      var instance = new Node(nodes[0]);
+      var instance = new BinarySearchTree(nodes[0]);
       instance.right = innerNodes(nodes.slice(1,  nodes.length));
       return instance
     }
   }
+  BST.left = innerNodes(array.slice(0, min))
+  BST.right = innerNodes(array.slice(min + 1, array.length))
   return BST
 }
 
 // Time: O(n)
 // Space: O(n)
+
+/* TEST */
+console.log(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
